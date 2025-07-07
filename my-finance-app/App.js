57046f70dc827e9af28959ac622798ec90fa14a1
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2025-07-06 21:41:21 -0700

    sprint 0 paso 2
    a punto de terminar con el sprint 0 solo que quedan algunas condiciones antes de poder considerarse terminado

--- my-finance-app/App.js
+++ my-finance-app/App.js
@@ -1,10 +1,11 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-2xl font-bold">¡Hola desde Tailwind!</Text>
       <StatusBar style="auto" />
     </View>
   );
