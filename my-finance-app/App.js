8c32708cb593f733623d2f76f82185721ceba492
enrique Gudiño <137984675+Catrin101@users.noreply.github.com>
2025-07-06 22:01:53 -0700

    sprint 0 paso 3
    se hizo el intento de arreglar la base de la aplicación sin mucho éxito

--- my-finance-app/App.js
+++ my-finance-app/App.js
@@ -1,11 +1,11 @@
 import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-2xl font-bold">¡Hola desde Tailwind!</Text>
+      <Text className="text-white text-2xl font-bold">¡Hola desde mi App de Finanzas!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3B82F6', // azul-500 de Tailwind
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
